d2ccd7d19c329aab4fb58377ebe0909fc07fd49c
Steven Epis <114670616+epissteven1@users.noreply.github.com>
2024-10-21 21:01:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ st.markdown(f"""
          background-color: transparent;
         }}
 
-        [data-testid="stAppViewContainer"] {{
+        [data-testid="stVerticalBlock"] {{
         background-color: #333333;
         }}
         [data-testid="stAppViewContainer"] {{
